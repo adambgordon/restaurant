c8d1c01698c012a8716f898f6142137adabf29ae
Adam Gordon <adambaumgordon@gmail.com>
2021-07-27 13:13:29 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import _ from 'lodash';
+import './style.css';
+
 console.log("This worked!");
+
+function component() {
+    const element = document.createElement('div');
+    element.textContent = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('content');
+    return element;
+}
+
+document.body.appendChild(component());
