fe0bb04d7203c3d49a59524736d7f4223b03f429
Adam Gordon <adambaumgordon@gmail.com>
2021-08-04 15:41:10 -0600

    update

--- src/initpage.js
+++ src/initpage.js
@@ -1,11 +1,10 @@
-import { fill, head } from "lodash";
 import * as helper from "./helper.js"
 
 function initHeader() {
     const header = document.createElement("div");
     header.id = "header";
     header.textContent = "the brunch place";
-    document.body.appendChild(header);
+    return header;
 }
 
 function initNav() {
@@ -14,31 +13,27 @@ function initNav() {
     nav.appendChild(helper.createTab("about"));
     nav.appendChild(helper.createTab("menu"));
     nav.appendChild(helper.createTab("contact"));
-    document.body.appendChild(nav);
+    return nav;
 }
 
 function initContent() {
     const content = document.createElement("div");
     content.id = "content";
-    document.body.appendChild(content);
-    helper.activate(document.getElementById("about"));
+    return content;
 }
 
 function initFooter() {
     const footer = document.createElement("div");
     footer.id = "footer";
-
     const copyright = document.createElement("dive");
-    copyright.textContent = "© 2021 adam gordon"
-
+    copyright.textContent = "©2021 adam gordon"
     footer.appendChild(copyright);
-    document.body.appendChild(footer);
+    return footer;
 }
 
 function initBackground() {
     const background = document.createElement("div");
     background.id = "background";
-
     const text = document.createElement("div");
     text.classList.add("background-text");
     let string = "the brunch place ";
@@ -47,15 +42,19 @@ function initBackground() {
     }
     text.textContent = string;
     background.appendChild(text);
-    document.body.appendChild(background);
+    return background;
 }
 
 function initPage () {
-    initHeader();
-    initNav();
-    initContent();
-    initFooter();
-    initBackground();
+    const wrapper = document.createElement("div");
+    wrapper.id = "wrapper";
+    wrapper.appendChild(initHeader());
+    wrapper.appendChild(initNav());
+    wrapper.appendChild(initContent());
+    wrapper.appendChild(initFooter());
+    wrapper.appendChild(initBackground());
+    document.body.appendChild(wrapper);
+    helper.activate(document.getElementById("about"));
 }
 
 export default initPage;
