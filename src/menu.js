3e5b501a98b46065cd5dd060f0267446fe6979f6
Adam Gordon <adambaumgordon@gmail.com>
2021-08-05 12:57:05 -0600

    update

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,44 @@
 import mains from "./menu/mains.csv";
 
 function createMenu() {
-    const item = document.createElement("div");
-    item.textContent = "food item food item food item"
-    console.log(mains[0][1]);
-    console.log(mains[1]);
-    // dynamically make css grid for each section of the menu
-    return item;
+    const menu = document.createElement("div");
+    const smallsHeader = document.createElement("div");
+    const smallsWrapper = document.createElement("div");
+    const mainsHeader = document.createElement("div");
+    const mainsWrapper = document.createElement("div");
+
+    menu.classList.add("menu");
+    smallsHeader.id = "smalls";
+    smallsWrapper.classList.add("smalls");
+    mainsHeader.id = "mains";
+    mainsWrapper.classList.add("mains");
+
+    smallsHeader.textContent = "smalls";
+    mainsHeader.textContent = "mains";
+
+    for (let i = 0; i < mains.length; i++) {
+        const dish = document.createElement("div");
+        const name = document.createElement("div");
+        const description = document.createElement("div");
+        
+        dish.classList.add("dish");
+        name.classList.add("dish-name");
+        description.classList.add("dish-description");
+
+        name.textContent = mains[i][0];
+        description.textContent = mains[i][1];
+        
+        dish.appendChild(name);
+        dish.appendChild(description);
+
+        mainsWrapper.appendChild(dish);
+    }
+
+    smallsHeader.appendChild(smallsWrapper);
+    mainsHeader.appendChild(mainsWrapper);
+    menu.appendChild(smallsHeader);
+    menu.appendChild(mainsHeader);
+    return menu;
 }
 
 export default createMenu;
