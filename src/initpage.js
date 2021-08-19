8e8ee4cac17740000fd43269a03ebe977c8f21ec
Adam Gordon <adambaumgordon@gmail.com>
2021-08-19 16:49:54 -0600

    update

--- src/initpage.js
+++ src/initpage.js
@@ -65,7 +65,7 @@ function initLogo() {
     const logoWrapper = document.createElement("div");
     logoWrapper.id = "logo-wrapper";
     logoWrapper.onclick = () => {
-        window.open("https://gbreadman.github.io/restaurant/","_blank");
+        window.open("https://gbreadman.github.io/restaurant/","_self");
     };
     const logoImage = new Image();
     logoImage.src = logo;
